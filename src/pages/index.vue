1cd2fe02745eb6eeeb1a09461baebf2df52ba038
SoundGM <sound.gm@proton.me>
2025-04-09 21:29:27 +0200

    Add font

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,7 +1,18 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div class="flex flex-col gap-4 p-10">
-    <span>hello world</span>
+  <div>
+    <div class="flex flex-col gap-4 p-10 font-light text-4xl">
+      <span>Whereas disregard and contempt for human rights have resulted</span>
+    </div>
+    <div class="flex flex-col gap-4 p-10 font-medium text-4xl">
+      <span>Whereas disregard and contempt for human rights have resulted</span>
+    </div>
+    <div class="flex flex-col gap-4 p-10 font-semibold text-4xl">
+      <span>Whereas disregard and contempt for human rights have resulted</span>
+    </div>
+    <div class="flex flex-col gap-4 p-10 font-bold text-4xl">
+      <span>Whereas recognition of the inherent dignity</span>
+    </div>
   </div>
 </template>
