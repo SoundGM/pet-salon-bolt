33634b7bc661b8095ce64cbd057f9ec72a2d492f
SoundGM <sound.gm@proton.me>
2025-05-18 17:57:01 +0200

    Updaet nuxt uito 3.1.2

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,11 +1,15 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+  import * as locales from '@nuxt/ui/locale'
+
+  const { locale } = useI18n()
+</script>
 
 <template>
   <div class="flex flex-col bg-secondary-400 min-h-screen">
     <LayoutAnnouncementBanner />
     <LayoutHeader />
     <!-- Added padding-top to account for banner (h-10) and header (h-16) -->
-    <UApp>
+    <UApp :locale="locales[locale]">
       <main class="flex-grow pt-26">
         <slot />
       </main>
